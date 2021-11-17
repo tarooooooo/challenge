fa7f25488980c3616f201c265864d03e1fbb83d5
tarooooooo <ryutaromaeda83@gmail.com>
2021-11-17 01:21:26 +0000

    [Add]ジャンル機能

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Genre.create(name:"勉強")
+Genre.create(name:"仕事")
+Genre.create(name:"家事")
+Genre.create(name:"趣味")
+Genre.create(name:"早起き")
+Genre.create(name:"健康・美容")
+Genre.create(name:"睡眠")
+Genre.create(name:"スポーツ")
+Genre.create(name:"ランニング")
+Genre.create(name:"トレーニング")
+Genre.create(name:"ウォーキング")
+Genre.create(name:"マインドフルネス")
