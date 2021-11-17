46b16016aa0553aca93f361887f034d2049ebc94
tarooooooo <ryutaromaeda83@gmail.com>
2021-11-17 08:57:06 +0000

    [Add]ジャンル画像の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_013004) do
+ActiveRecord::Schema.define(version: 2021_11_17_080210) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
   create_table "posts", force: :cascade do |t|
